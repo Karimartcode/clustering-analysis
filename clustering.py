057d4883390fd8ab28ba3033a0366e7b52976d9b
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2023-10-14 15:26:41 +0000

    add standardization

--- clustering.py
+++ clustering.py
@@ -5,3 +5,10 @@ from sklearn.datasets import make_blobs
 def generate_data(n_samples=300, n_centers=4, seed=42):
     X, y = make_blobs(n_samples=n_samples, centers=n_centers, random_state=seed, cluster_std=1.0)
     return X, y
+
+
+from sklearn.preprocessing import StandardScaler
+
+def standardize(X):
+    scaler = StandardScaler()
+    return scaler.fit_transform(X), scaler
